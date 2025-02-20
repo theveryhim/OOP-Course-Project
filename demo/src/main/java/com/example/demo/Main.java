e135e55d79f6bc5608f6e31ba5caaefe6eb4edd4
Arman Yazdani <finger81dancer@gmail.com>
2025-02-20 22:14:01 +0330

    delete demo

--- demo/src/main/java/com/example/demo/Main.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-public class Main {
-    public static void main(String[] args){
-        Controller programController = new Controller();
-        programController.run();
-    }
-}
